5c4e21f960bd42a3f2959710263465bf29e11777
simomarsili <simomarsili@gmail.com>
2019-01-18 12:07:21 +0100

    add project_name to __init__.py

--- project/package1/__init__.py
+++ project/package1/__init__.py
@@ -2,7 +2,8 @@ import pkg_resources
 
 
 # the name of the project from the setup.py
-__version__ = pkg_resources.require('project_name')[0].version
+project_name = 'project_name'
+__version__ = pkg_resources.require(project_name)[0].version
 __copyright__ = 'Copyright (C) 2019 Simone Marsili'
 __license__ = 'BSD 3 clause'
 __author__ = 'Simone Marsili (simo.marsili@gmail.com)'
