4e62a1bcede67ca119fc4ef70b0337ee48c3b3bb
simomarsili <simomarsili@gmail.com>
2019-01-18 11:47:29 +0100

    get version via pkg_resources

---
+++ project/package1/__init__.py
@@ -0,0 +1,9 @@
+import pkg_resources
+
+
+# the name of the project from the setup.py
+__version__ = pkg_resources.require('project_name')[0].version
+__copyright__ = 'Copyright (C) 2019 Simone Marsili'
+__license__ = 'BSD 3 clause'
+__author__ = 'Simone Marsili (simo.marsili@gmail.com)'
+__all__ = []
