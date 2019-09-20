634c909f6e6c6491263c88f1fe5e9f222e83fb2c
simomarsili <simo.marsili@gmail.com>
2019-09-20 16:41:28 +0200

    update init

--- project/package/__init__.py
+++ project/package/__init__.py
@@ -1,10 +1,8 @@
+"""kagtools package."""
+import os
 import pkg_resources
 
-
-# the name of the project from the setup.py
-project_name = 'project_name'
-__version__ = pkg_resources.require(project_name)[0].version
-__copyright__ = 'Copyright (C) 2019 Simone Marsili'
-__license__ = 'BSD 3 clause'
-__author__ = 'Simone Marsili (simo.marsili@gmail.com)'
-__all__ = []
+SEP = os.path.sep
+package_path = os.path.dirname(os.path.abspath(__file__))
+package_name = package_path.split(SEP)[-1]
+__version__ = pkg_resources.require(package_name)[0].version
