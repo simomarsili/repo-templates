284f363fc73fc0cbdc194282394e6847ccbced69
simomarsili <simo.marsili@gmail.com>
2020-05-07 19:58:52 +0200

    single top-level package; same name as the project

--- project/setup.py
+++ project/setup.py
@@ -1,17 +1,53 @@
 # -*- coding: utf-8 -*-
-"""Setup template."""
+"""
+Setup template.
+The repo should contain a single top-level package or module
+ **WITH THE SAME NAME OF THE PROJECT**.
+Any additional packages and modules are contained in the top level package.
+"""
 import codecs
 import json
 from os import path
 from setuptools import setup
-# from setuptools import find_packages
+from setuptools import find_packages
 
-# the project and the top-level package have the same name
-PROJECT_NAME = 'my_project'
-PACKAGES = ['package', 'package_2']
-# PACKAGES = find_packages(exclude=['tests'])
-MODULES = ['module']
-PACKAGE_FILE = 'package.json'
+
+def get_package_info(source):
+    """ Retrieve package info."""
+    with open(source, 'r') as fp:
+        info = json.load(fp)
+    try:
+        version = info['version']
+    except KeyError:
+        # no version number in version.json
+        raise KeyError("check package.json file: no version number")
+    try:
+        name = info['name']
+    except KeyError:
+        # no version number in version.json
+        raise KeyError("check package.json file: no project name")
+    return name, version
+
+
+def get_long_description(readme):
+    """Get the long description from the README file."""
+    with codecs.open(readme, encoding='utf-8') as _rf:
+        return _rf.read()
+
+
+HERE = path.abspath(path.dirname(__file__))
+# package.json and README are in the project root
+PACKAGE_FILE = path.join(HERE, 'package.json')
+README_FILE = path.join(HERE, 'README.rst')
+
+
+PROJECT_NAME, VERSION = get_package_info(PACKAGE_FILE)
+LONG_DESCRIPTION = get_long_description(README_FILE)
+
+PACKAGES = find_packages(exclude=['tests'])
+# PACKAGES = []
+# MODULES = []
+
 SETUP_REQUIRES = []
 INSTALL_REQUIRES = [
     # # this is an example of URL based requirement (see PEP508):
@@ -19,27 +55,6 @@ INSTALL_REQUIRES = [
 ]
 EXTRAS_REQUIRES = {'test': ['pytest']}
 
-
-def get_version(source):
-    """ Retrieve version number."""
-    with open(source, 'r') as fp:
-        version_data = json.load(fp)
-    try:
-        return version_data['version']
-    except KeyError:
-        # no version number in version.json
-        raise KeyError("check version file: no version number")
-
-
-def get_long_description(here):
-    """Get the long description from the README file."""
-    with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as _rf:
-        return _rf.read()
-
-
-HERE = path.abspath(path.dirname(__file__))
-VERSION = get_version(path.join(HERE, PACKAGE_FILE))
-LONG_DESCRIPTION = get_long_description(HERE)
 
 setup(
     name=PROJECT_NAME,
@@ -49,7 +64,7 @@ setup(
     author='Simone Marsili',
     author_email='simo.marsili@gmail.com',
     url='https://github.com/simomarsili/' + PROJECT_NAME,
-    py_modules=MODULES,
+    # py_modules=MODULES,
     packages=PACKAGES,
     setup_requires=SETUP_REQUIRES,
     install_requires=INSTALL_REQUIRES,
