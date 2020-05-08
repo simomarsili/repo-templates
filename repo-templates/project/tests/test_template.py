f220ef7a987ebc4f9aa6658fc2378f92de69c7b3
simomarsili <simo.marsili@gmail.com>
2020-05-08 17:12:18 +0200

    pylit

--- repo-templates/project/tests/test_template.py
+++ repo-templates/project/tests/test_template.py
@@ -1,15 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Tests."""
+import os
+
+
 def tests_dir():
     """Return None is no tests dir."""
-    import os
     cwd = os.getcwd()
     basename = os.path.basename(cwd)
     if basename == 'tests':
         return cwd
-    else:
-        tests_dir = os.path.join(cwd, 'tests')
-        if os.path.exists(tests_dir):
-            return tests_dir
+    tdir = os.path.join(cwd, 'tests')
+    if os.path.exists(tdir):
+        return tests_dir
+    return None
 
 
 def test_0():
-    assert 1 == 1
+    """Test template."""
+    assert bool(1) is True
