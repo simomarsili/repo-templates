b5fa5b9264194b1f1ce9fd0a845acec60ecd7f28
simomarsili <simo.marsili@gmail.com>
2020-05-08 14:32:15 +0200

    single top-level package

--- project/setup.py
+++ project/setup.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Setup template.
-The repo should contain a single top-level package or module
- **WITH THE SAME NAME OF THE PROJECT**.
+Setup for repo template.
+The project should contain a **single top-level package**.
 Any additional packages and modules are contained in the top level package.
 """
+
 import codecs
 import json
 from os import path
@@ -29,18 +29,27 @@ def get_long_description(readme):
         return _rf.read()
 
 
+def get_package_name():
+    top_level_packages = [p for p in find_packages(exclude=['tests'])
+                          if '.' not in p]
+    if len(top_level_packages) != 1:
+        raise ValueError('Project must contain a single top-level package.')
+    return top_level_packages[0]
+
+
 HERE = path.abspath(path.dirname(__file__))
-# project.json and README are in the project root
 PROJECT_FILE = path.join(HERE, 'project.json')
 README_FILE = path.join(HERE, 'README.rst')
 
-
 project_info = get_info(PROJECT_FILE)
+
+project_name = project_info['name']
+version = project_info['version']
+package_name = get_package_name()
 long_description = get_long_description(README_FILE)
 
-PACKAGES = find_packages(exclude=['tests'])
-# PACKAGES = []
-# MODULES = []
+packages = find_packages(exclude=['tests'])
+modules = []
 
 SETUP_REQUIRES = []
 INSTALL_REQUIRES = [
@@ -49,20 +58,32 @@ INSTALL_REQUIRES = [
 ]
 EXTRAS_REQUIRES = {'test': ['pytest']}
 
+print(
+    HERE,
+    PROJECT_FILE,
+    README_FILE,
+    project_info,
+    project_name,
+    version,
+    package_name,
+    packages
+)
 
 setup(
-    name=project_info['name'],
-    version=project_info['version'],
+    name=project_name,
+    version=version,
     description='A template project with packages',
     long_description=long_description,
     author='Simone Marsili',
     author_email='simo.marsili@gmail.com',
-    url='https://github.com/simomarsili/' + project_info['name'],
-    # py_modules=MODULES,
-    packages=PACKAGES,
+    url='https://github.com/simomarsili/' + project_name,
+    py_modules=modules,
+    packages=packages,
     setup_requires=SETUP_REQUIRES,
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRES,
+    package_data={'my_project': ['project.json']},
+    include_package_data=True,
     license='BSD 3-Clause',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
